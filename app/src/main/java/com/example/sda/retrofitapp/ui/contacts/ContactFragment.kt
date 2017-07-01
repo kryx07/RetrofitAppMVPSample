95476821cdb35ceea3ef66c6089e66bf357426cf
kryx07 <kryx07@gmail.com>
2017-07-01 10:16:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sda/retrofitapp/ui/contacts/ContactFragment.kt
@@ -0,0 +1,16 @@
+package com.example.sda.retrofitapp.ui.contacts
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by sda on 01.07.17.
+ */
+class ContactFragment : Fragment(), ContactsMvpView {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
